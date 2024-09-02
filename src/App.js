5f5f7eb6196e0a52db8ea4567f1de7f140a9283e
payal patel <jaydipa94patel@gmail.com>
2024-09-02 21:53:32 +0530

    react full stack projrct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import{BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import FooterComponent from './Component/FooterComponent';
+import HeaderComponent from './Component/HeaderComponent';
+import ListEmployeeComponent from './Component/ListEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <HeaderComponent/>
+          <div className="container">
+      <switch>
+        <Route path="/" Component={ListEmployeeComponent}></Route>
+        <Route path="/employees" Component={ListEmployeeComponent}></Route>
+          <ListEmployeeComponent/>
+        </switch>
+        </div>
+    <FooterComponent/>
+    </Router>
     </div>
   );
 }
